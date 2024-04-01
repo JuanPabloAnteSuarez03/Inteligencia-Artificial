5afad7d449da64f5c67d21ede2a1d96beb4cb656
JuanPabloAnteSuarez03 <juan.pablo.ante@correounivalle.edu.co>
2024-04-01 18:40:18 -0500

    Garcés

--- Rata_distancia_euclideana.py
+++ Rata_distancia_euclideana.py
@@ -56,6 +56,10 @@ def encontrar_camino_mas_corto(matriz):
     movimientos_realizados = []  
     primer_mov = (encontrar_fila_rata(matriz), encontrar_columna_rata(matriz))
     movimientos_realizados.append(primer_mov)
+    contador_movimientos = 0  # Inicializar contador de movimientos
+    costo_total_movimiento = 0  # Inicializar el costo total de movimiento
+    paso_por_5 = False  # Variable para rastrear si la rata ha pasado por un 5
+    contador_pasos = 0  # Variable para contar los movimientos después de que paso_por_5 sea True
     while True:
         movimientos = movimientos_posibles(matriz)
         if not movimientos:
@@ -82,13 +86,27 @@ def encontrar_camino_mas_corto(matriz):
             matriz[ultimo_movimiento[0]][ultimo_movimiento[1]] = 0  
         
         movimientos_realizados.append(mejor_movimiento)  
+        contador_movimientos += 1  # Incrementar contador de movimientos
+        
+        if paso_por_5:
+            costo_total_movimiento += 1  # Aumentar el costo total de movimiento en 1 si la rata ha pasado por un 5
+            contador_pasos += 1  # Incrementar contador de pasos después de pasar por un 5
+            if contador_pasos >= 10:  # Si ha pasado por 10 movimientos después de un 5, restablecer paso_por_5 a False
+                paso_por_5 = False
+                contador_pasos = 0
+        else:
+            costo_total_movimiento += 2  # Si no ha pasado por un 5, aumentar en 2
 
         if matriz[mejor_movimiento[0]][mejor_movimiento[1]] == 3:
             print("La rata ha llegado al queso.")
-            print("La cantidad minima de movimientos es", len(movimientos_realizados[1:]))
+            print("La cantidad mínima de movimientos es:", contador_movimientos)
             print("El camino a tomar es el siguiente:", movimientos_realizados[1:])
+            print("El costo total del recorrido es:", costo_total_movimiento)  # Mostrar el costo total
             movimientos_realizados.append(mejor_movimiento)
             return 
+        
+        if matriz[mejor_movimiento[0]][mejor_movimiento[1]] == 5:
+            paso_por_5 = True
         
         matriz[mejor_movimiento[0]][mejor_movimiento[1]] = 1  
         
@@ -98,9 +116,9 @@ def encontrar_camino_mas_corto(matriz):
         print(movimientos_realizados[1:])
 
 matriz = [
-    [0, 0, 0, 0, 3],
-    [0, 0, 2, 2, 0],
-    [0, 1, 2, 0, 0],
+    [1, 2, 0, 0, 3],
+    [5, 2, 2, 0, 0],
+    [0, 0, 2, 0, 0],
     [0, 0, 2, 0, 0],
     [0, 0, 0, 0, 0]
 ]
