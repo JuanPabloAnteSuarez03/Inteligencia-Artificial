13059855fab11336fa43c1aa5e2a289758a155ff
Alejandro Guerrero Cano <alejogc2002@hotmail.com>
2024-04-08 22:25:45 -0500

    2° avance de busqueda por profundidad

--- modelo/busqueda.py
+++ modelo/busqueda.py
@@ -18,7 +18,7 @@ def reconstruir_camino(nodo):
     camino.reverse()  # Invertir el camino para obtener el orden correcto
     return camino
 
-def evitar_ciclo(nodo):
+def evitar_ciclos(nodo):
     if nodo.profundidad <= 1:
         return False
     antecesor = nodo.padre.padre
@@ -33,6 +33,7 @@ def evitar_ciclo(nodo):
 def busqueda_dfs(ambiente):
     nodo = Nodo(ambiente)
     stack = [nodo]  # Usar una pila para almacenar nodos a explorar
+    explorados = set()  # Conjunto para almacenar estados explorados
     
     while stack:
         nodo_actual = stack.pop()  # Obtener el nodo más reciente de la pila
@@ -40,22 +41,27 @@ def busqueda_dfs(ambiente):
         if es_nodo_meta(nodo_actual):
             return reconstruir_camino(nodo_actual), "Se encontró"
         
-        if nodo_actual.profundidad >= 100:  # Evitar bucles infinitos
-            continue
+        
+        
+        estado_actual = str(nodo_actual.estado.matriz)  # Convertir la matriz a una cadena para usarla como clave
+        
+        if estado_actual in explorados or evitar_ciclos(nodo_actual):
+            continue  # Evitar nodos ya explorados y ciclos
+        
+        explorados.add(estado_actual)  # Agregar el estado actual al conjunto de explorados
         
         for accion in nodo_actual.estado.mando.get_movimientos_posibles(nodo_actual.estado.matriz):
             nuevo_estado = nodo_actual.estado.copy()
             nuevo_estado.transicion(accion)
             nuevo_nodo = Nodo(nuevo_estado, nodo_actual, accion, nodo_actual.profundidad + 1)
             
-            if not evitar_ciclo(nuevo_nodo):
-                stack.append(nuevo_nodo)  # Agregar el nuevo nodo a la pila
+            stack.append(nuevo_nodo)  # Agregar el nuevo nodo a la pila
     
     return [], "No se encontró"
 
 # Cargar el ambiente desde el archivo
 ambiente = Ambiente()
-ambiente.cargar_desde_archivo(r'modelo\ambientebasico.txt')
+ambiente.cargar_desde_archivo(r'modelo\ambiente.txt')
 
 # Realizar la búsqueda DFS
 inicio = time.time()
