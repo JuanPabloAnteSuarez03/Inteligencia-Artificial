843b48d01b47eb2e5f859fe989af4aa953bc01a1
Alejandro Guerrero Cano <alejogc2002@hotmail.com>
2024-04-11 22:12:54 -0500

    Avance de vistas, ya muestra información

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import sys
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from vista.ventanaInicio import Ui_Busqueda
+from vista.ventanaInicio import *
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ventana_inicio = QMainWindow()
-    ui = Ui_Busqueda()
-    ui.setupUi(ventana_inicio)
+    app = QtWidgets.QApplication(sys.argv)
+    ventana_inicio = VentanaInicio()
     ventana_inicio.show()
     sys.exit(app.exec_())
